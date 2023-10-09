96fe0151fe45fec04c431c8cdd5c3b6b6338a468
IkrizzRama <147387755+IkrizzRama@users.noreply.github.com>
2023-10-09 16:33:48 +0700

    Add files via upload

---
+++ Lab 00/DENS_E_2306240143_MuhammadRizkyRamadhani_Lab0.zip/refleksi_lab0.py
@@ -0,0 +1,19 @@
+#Mencetak judul dari program ini
+print("Perkenalan diri anak pacil baru")
+
+#Melakukan input untuk mendapatkan informasi yang dibutuhkan
+nama_pacil = input("Masukkan nama kamu: ")
+asal_jurusan = input("Masukkan jurusan kamu: ")
+tanggal_lahir = input("Masukkan tanggal lahir kamu: ")
+asal_kota = input("Masukkan asal kota kamu: ")
+asal_sekolah = input("Masukkan asal sekolah kamu: ")
+motto_hidup = input("Masukkan motto hidup kamu: ")
+
+#Pembatas untuk membuat output terlihat lebih rapih
+print("------------------------------------------")
+
+#Mencetak ucapan perkenalan diri berdasarkan data yang telah diinput
+print ("Halo " + nama_pacil + " dari Jurusan " + asal_jurusan +" lahir pada " + tanggal_lahir)
+print ("Berasal dari " + asal_kota + " dan asal sekolah " + asal_sekolah)
+print ("Memiliki motto hidup " + motto_hidup)
+print ("Selamat bergabung di keluarga Fasilkom!")
